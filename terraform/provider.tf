77c96092db2018c5a525dc250ba6b627cd45ac73
RojanShrestha1 <shrestarojan099@gmail.com>
2026-04-05 08:14:59 +0545

    Finalizing CI/CD with S3 Backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,15 @@
 terraform {
+  backend "s3" {
+    bucket         = "gradle-state-129718466065-ap-south-1-an"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+
+  }
+}
+
+
+
+terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
